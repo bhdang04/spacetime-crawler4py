9a61aabf4ae36fe5a2f17a705d2546d307a40393
YTJJGamingg <jgacs@uci.edu>
2026-01-31 06:16:16 +0000

    fixed something

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin, urldefrag
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -8,13 +9,38 @@ def scraper(url, resp):
 def extract_next_links(url, resp):
     # Implementation required.
     # url: the URL that was used to get the page
+    base_url = url
     # resp.url: the actual url of the page
+    actual_url = getattr(resp, "url", None)
     # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    if resp is None or resp.status != 200:
+        return []
     # resp.error: when status is not 200, you can check the error here, if needed.
+    error_msg = getattr(resp, "error", None)
     # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
+    if resp.raw_response is None:
+        return []
+    raw_url = getattr(resp.raw_response, "url", None)
+    html_bytes = getattr(resp.raw_response, "content", None)
+    if not html_bytes:
+        return []
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    links = []
+    try: 
+        soup = BeautifulSoup(html_bytes, "lxml")
+    except Exception:
+        return []
+    for tag in soup.find_all("a", href=True):
+        href = tag.get("href")
+        if not href:
+            continue
+        absolute_url = urljoin(base_url, href)
+        absolute_url, _ = urldefrag(absolute_url)
+        links.append(absolute_url)
+    return links
+
     return list()
 
 def is_valid(url):
