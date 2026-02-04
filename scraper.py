1ea608574589d524b83e37cccb11a5873b5e7e35
YTJJGamingg <jgacs@uci.edu>
2026-02-04 00:47:51 -0800

    Updated two report functions 3&4 top 50 common words and top subdomain

--- scraper.py
+++ scraper.py
@@ -1,16 +1,42 @@
 import re
 from bs4 import BeautifulSoup
+from collections import Counter, defaultdict
 from urllib.parse import urlparse, urljoin, urldefrag
 
 visited = set() # URLs we have successfully processed
-blacklist = set() #URLs we deicded traps/ bad content
+blacklist = set() #URLs we decided traps/ bad content
+url_depths = {}
+
+#Analytics
+Common_Words = Counter()
+Subdomains = defaultdict(int)
+
+Stop_Words = {
+    "a","an","and","are","as","at","be","but","by","for","from","has","have",
+    "he","her","hers","him","his","i","if","in","into","is","it","its","me",
+    "my","not","of","on","or","our","ours","she","so","that","the","their",
+    "theirs","them","then","there","these","they","this","those","to","us",
+    "was","we","were","what","when","where","which","who","why","will","with",
+    "you","your","yours"
+}
+
+##########
+#HELPER###
+##########
+
+def defrag_url(u):
+    if not u:
+        return None
+    u = u.strip()
+    u, _frag = urldefrag(u)
+    return u
 
 def scraper(url, resp):
-    links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    curr_depth = url_depths.get(url, 0)
+    links = extract_next_links(url, resp, curr_depth)
+    return [link for link, depth in links if is_valid(link, depth)]
 
-
-def extract_next_links(url, resp):
+def extract_next_links(url, resp, curr_depth=0):
     final_links = []
 
     #Remove URL fragments so uniqueness stays #'s
@@ -18,8 +44,7 @@ def extract_next_links(url, resp):
     if not url:
         return final_links
 
-
-    #If we've sene or rejected URL why process it?
+    #If we've seen or rejected URL why process it?
     if url in visited or url in blacklist or resp is None or resp.raw_response is None:
         blacklist.add(url)
         return final_links
@@ -28,6 +53,7 @@ def extract_next_links(url, resp):
     status = getattr(resp.raw_response, "status_code", None)
     if status is not None and status >= 400:
         blacklist.add(url)
+        print(status)
         return final_links
 
     #Examine the current page path for known trap sections
@@ -39,19 +65,23 @@ def extract_next_links(url, resp):
         blacklist.add(url)
         return final_links
 
-    visited.add(url)
-
     #WordPress upload directory = binary files
     if "/wp-content/" in path_lower:
         blacklist.add(url)
         return final_links
-
     
     #This is the point where a page is considered safe to process
     visited.add(url)
 
     #Creates a BeautifulSoup object named soup using the html content (resp.raw_response.content)
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+    update_common_words_from_soup(soup)
+    update_subdomain_count(url)
+
+    if len(visited) % 10 == 0:
+        write_top_50()
+        write_subdomains()
 
     #For every link found from scraping the web page
     for link in soup.find_all('a'):
@@ -64,41 +94,51 @@ def extract_next_links(url, resp):
         if not href:
             continue
 
-        #If valid, passes it to the final_links list
-        absolute = urljoin(url,href)
-        absolute = defrag_url(absolute)
-        if not absolute:
-            continue
-
-        if absolute in visited or absolute in blacklist:
-            continue
+        #This block catches URLs that have correct href input types (ie. contain a hyperlink), but 
+        #don't lead to a valid IP or destination / aren't in the right format.
+        try:
+            absolute = urljoin(url,href)
+            absolute = defrag_url(absolute)
             
-        if is_valid(absolute):
-            final_links.append(absolute)
+            if not absolute or absolute in visited or absolute in blacklist:
+                continue
+
+            new_depth = curr_depth + 1
+
+            if is_valid(absolute, new_depth):
+                url_depths[absolute] = new_depth
+                final_links.append((absolute, new_depth))
+        except ValueError:
+            continue
+        
     return final_links
 
-def defrag_url(u):
-    if not u:
-        return None
-    u = u.strip()
-    u, _frag = urldefrag(u)
-    return u
 
-def is_valid(url):
+def is_valid(url, depth=0, max_depth=10):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
 
+    # Implemented a Depth Checker for unique links
+    if depth > max_depth:
+        return False
+    
     #What this does so far is it checks whether the URL passed through the parameter is valid or not.
     try:
+        #if no url is passed, return false.
         if not url:
             return False
         
-        #Here we create an allow variable
+        #Here we create an allow variable.
         allow = False
 
         #We parse the given url into a parsed object, which contains its domain, path, etc.
         parsed = urlparse(url)
+
+        #If the parsed object contains a query component, mark the URL as invalid.
+        #This is because query pages often lead to traps, due to generating an infinite amount of URLs.
+        if parsed.query:
+            return False
 
         #A set containing all the valid domains we can crawl for this assignment.
         valid_domains = set(["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"])
@@ -112,9 +152,9 @@ def is_valid(url):
         
         #For every domain in the valid domains list, if the domain from parsed
         #ends with a valid domain name, we set allow to true and break the loop.
-        #This means that the URL is valid and we're allowed to crawl it
+        #This means that the URL is valid and we're allowed to crawl it.
         for domain in valid_domains:
-            if parsed_domain.endswith(domain):
+            if parsed_domain == domain or parsed_domain.endswith("." + domain):
                 allow = True
                 break
         
@@ -122,7 +162,7 @@ def is_valid(url):
         if not allow:
                 return False
 
-        # Noticable trap patterns discovered when running
+        # Noticable trap patterns discovered when running.
         #trap_patterns contains a list of regex strings which will be used later on to 
         #check if the URL is a trap.
         #For example, a calendar is likely to be indicated by the date format "\d{4}-\d{2}-\d{2}"
@@ -134,11 +174,18 @@ def is_valid(url):
             r".*tribe-bar-date.*",
             r".*share.*", 
             r".*ical.*", 
+            r".*/page/\d+.*", #Catches URLs that have multiple pages that the crawler gets stuck in. \d represents any digit 0-9.
+            r".*/tree/.*", #Catches git trees that trap crawler.
+            r".*/commits?/.*", #Catches git commits.
+            r".*/branch(?:es)?/.*", #Catches git branches and branch URLs.
             r".*wp-login.*",
             r".*replytocom.*", # Common WordPress comment trap
             r".*action=.*",      # Catches compose, template, etc.
             r".*[\?&;]c=[dnsm].*[\?&;]o=[ad].*", #catches C=D;O=A ?C=N; O=D
             r".*/author/.*/page/\d+.*", #Ends the WICS crawler trap
+            r".*/login.*", #Get rid of login pages
+            r".*/activity.*", # Found in logs returning 608
+            r".*/projects.*", # Found in logs returning 608
         ]
         
         #Checks if any of the trap_patterns strings are located inside of the url.
@@ -158,8 +205,11 @@ def is_valid(url):
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
                return False
         
+        #If the length of the URL is larger than 300, the URL is invalid.
         if len(url) > 300:
             return False
+        
+        #If the URL contains more than 10 "&", we return false.
         if url.count("&") > 10:
             return False
         
@@ -169,3 +219,48 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+##########
+#ANALYTICS
+##########
+
+
+def tokenize_A1(text: str):
+    clean_chars = []
+    for char in text:
+        if (char.isascii() and char.isalnum()) or char.isspace():
+            clean_chars.append(char)
+        else:
+            clean_chars.append(" ")
+    clean_string = "".join(clean_chars).lower()
+    return clean_string.split()
+
+
+def update_common_words_from_soup(soup: BeautifulSoup):
+    for tag in soup(["script", "style", "noscript"]):
+        tag.decompose()
+
+    text = soup.get_text(separator=" ")
+    tokens = tokenize_A1(text)
+
+    for t in tokens:
+        if t not in Stop_Words and len(t) > 2 and not t[0].isdigit():
+            Common_Words[t] += 1
+
+def update_subdomain_count(url: str):
+    parsed = urlparse(url)
+    host = (parsed.hostname or "").lower()
+
+    if host.endswith("uci.edu"):
+        Subdomains[host] += 1
+
+
+def write_top_50(out_file="top50_words.txt"):
+    with open(out_file, "w", encoding="utf-8") as f:
+        for w, c in Common_Words.most_common(50):
+            f.write(f"{w}, {c}\n")
+
+def write_subdomains(out_file="subdomains.txt"):
+    with open(out_file, "w", encoding="utf-8") as f:
+        for host in sorted(Subdomains):
+            f.write(f"{host}, {Subdomains[host]}\n")
